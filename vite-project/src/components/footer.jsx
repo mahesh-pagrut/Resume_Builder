f4a3ca09c8834926a86d5d3e98f63ae76c67dac4
mahesh-pagrut <maheshdpmayshu@gmail.com>
2024-12-20 18:57:42 +0530

    add file

--- vite-project/src/components/footer.jsx
+++ vite-project/src/components/footer.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Footer({ numOfHours }) {
     return (
-        <footer style={{color:"white", backgroundColor:"gray", padding:"5px"}}>
-            <p>&copy;  Mavericks the Ultimate Resume Builder</p>
+        <footer style={{color:"gray", padding:"10px",boxShadow: "rgba(0, 0, 0, 0.52) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px"}}>
+            <p>&copy;Mavericks the Ultimate Resume Builder</p>
+            
         </footer>
     );
 }
